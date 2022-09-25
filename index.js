d98095ac9bcce9302b4b6d3074eef88fd8f6a1d5
Daniel Hiep Truong <danieltruong89@yahoo.com>
2022-09-25 17:41:54 -0600

    commit tree a30ccd535bd3f3040063e29d975b871830ed20ca

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+let employee = {
+ name: "Daniel",
+ streetAddress: "1320 Stuy Town",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+ const newObj = {...obj};
+ newObj[key] = value;
+ return newObj
+}
+
+const newEmployee = updateEmployeeWithKeyandValue(obj, "name", "Jazzy");
+
+console.log(obj)
+console.log(newEmployee)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+ obj[key] = value;
+ return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+ const deleteKey = {...employee}
+ delete deleteKey[key]
+ return deleteKey
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+ delete employee[key];
+ return employee;
+}
